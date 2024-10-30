13cc704b342f9448bf99d911db401b198e39374b
a-mancodesss <079bct011.aman@pcampus.edu.np>
2024-10-30 18:54:41 +0545

    update post default value

--- src/app/updatePost/page.jsx
+++ src/app/updatePost/page.jsx
@@ -1,9 +1,11 @@
 import './updatePost.css'
 import { updatePost } from '@/database/action'
+import { getPost } from '@/database/data'
 import { auth } from '@/lib/auth'
 const page = async({searchParams}) => {
 const session = await auth()
 const {id}= searchParams
+const post = await getPost(id)
 const author = session?.user?.name
 // console.log('post before dispatch: ',id)
   return (
@@ -13,11 +15,11 @@ const author = session?.user?.name
 
         <h1 className='text-2xl font-bold'>Create Post</h1>
         <input type="hidden" name="id" value={id} />
-        <input type="text" name="title" id="title" placeholder='Title' />
-        <input type="text" name="description" id="desc" placeholder='Description...' />
-        <input type="text" name="imgUrl" id="imgUrl" placeholder='Image Url' />
+        <input type="text" name="title" id="title" placeholder='Title' defaultValue={post.title} />
+        <input type="text" name="description" id="desc" placeholder='Description...' defaultValue={post.description} />
+        <input type="text" name="imgUrl" id="imgUrl" placeholder='Image Url' defaultValue={post.imagUrl} />
         <input type="hidden" value={author} name="userId" id="userId" placeholder='Author' />
-        <button type="submit" className='bg-blue-950 w-full text-white py-2'>Submit</button>
+        <button type="submit" className='bg-blue-950 w-full text-white py-2'>Update</button>
 
     
     </form>
