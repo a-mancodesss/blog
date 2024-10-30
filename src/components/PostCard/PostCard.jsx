4524b59d67990de34fca42745d404f2525e1f1cf
Aman Partel <079bct011.aman@pcampus.edu.np>
2024-10-30 23:53:30 +0545

    Update PostCard.jsx

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -7,22 +7,22 @@ const PostCard = ({ title, author, description, imgUrl,slug }) => {
     <>
       <div className="card-container flex flex-col sm:flex-row  items-center">
         
-        <div className="group max-w-fit border  shadow-sm  shadow-blue-700/[50%] rounded-md px-2 py-4 sm:max-w-fit flex flex-col sm:flex-row sm:h-max cursor-pointer gap-y-4">
+        <div className="group max-w-fit border  shadow-sm  shadow-blue-700/[50%] rounded-md px-2 py-4  flex flex-col cursor-pointer gap-y-4">
           <div className="top w-[315px] overflow-hidden  relative">
             <div className=" transition-all duration-300 image-container relative h-[150px] w-[300px] sm:h-[300px] mx-auto">
               <Image className="object-cover rounded-md " alt="image" src={imgUrl?imgUrl:"https://dummyimage.com/600x400/000/fff"} fill priority/>
             </div>
           </div>
 
-          <div className="bottom w-[90%] sm:w-auto mx-auto flex flex-col ">
+          <div className="bottom w-[90%] mx-auto flex flex-col  ">
             <div className="left ">
               <div className=" text-container sm:w-[200px] flex flex-col flex-wrap ">
                 <div className="text-xl font-bold mb-2">{title}</div>
                 <div className="text-gray-500">{author}</div>
-                <p className="hidden">{description}</p>
+                <p className="hidden sm:block">{description}</p>
               </div>
             </div>
-            <div className="right flex justify-center bg-slate-900 rounded-md text-white font-semibold mt-2">
+            <div className="right py-2 flex justify-center bg-slate-900 rounded-md text-white font-semibold mt-2">
               <Link href={`blog/${slug}`} className="">
                 Read more ---
               </Link>
